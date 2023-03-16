349eab01094469d8fadf19fa384f0642e2cefac4
Emanuele Cesena <ecesena@jumpcrypto.com>
2023-03-15 22:15:29 -0500

    Add compile-time feature to replace Poseidon with Poseidon2

--- plonky2/src/gates/mod.rs
+++ plonky2/src/gates/mod.rs
@@ -11,8 +11,14 @@ pub mod gate;
 pub mod multiplication_extension;
 pub mod noop;
 pub mod packed_util;
+#[cfg(feature = "poseidon1")]
 pub mod poseidon;
+#[cfg(feature = "poseidon1")]
 pub mod poseidon_mds;
+#[cfg(not(feature = "poseidon1"))]
+pub mod poseidon2;
+#[cfg(not(feature = "poseidon1"))]
+pub mod poseidon2_mds;
 pub mod public_input;
 pub mod random_access;
 pub mod reducing;
@@ -24,3 +30,8 @@ pub mod util;
 // See https://github.com/rust-lang/cargo/issues/8379
 #[cfg(any(feature = "gate_testing", test))]
 pub mod gate_testing;
+
+#[cfg(not(feature = "poseidon1"))]
+pub use poseidon2 as poseidon;
+#[cfg(not(feature = "poseidon1"))]
+pub use poseidon2_mds as poseidon_mds;
--- plonky2/src/hash/mod.rs
+++ plonky2/src/hash/mod.rs
@@ -5,5 +5,16 @@ pub mod keccak;
 pub mod merkle_proofs;
 pub mod merkle_tree;
 pub mod path_compression;
+#[cfg(feature = "poseidon1")]
 pub mod poseidon;
+#[cfg(feature = "poseidon1")]
 pub mod poseidon_goldilocks;
+#[cfg(not(feature = "poseidon1"))]
+pub mod poseidon2;
+#[cfg(not(feature = "poseidon1"))]
+pub mod poseidon2_goldilocks;
+
+#[cfg(not(feature = "poseidon1"))]
+pub use poseidon2 as poseidon;
+#[cfg(not(feature = "poseidon1"))]
+pub use poseidon2_goldilocks as poseidon_goldilocks;
